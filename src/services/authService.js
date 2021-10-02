ffb8271e8ba4032b2d6a3b4726d4d9ed6059b1eb
sameed <sameed@sameedatif@hotmail.com>
2021-10-03 00:20:22 +0500

    Removed trailing debugger

--- src/services/authService.js
+++ src/services/authService.js
@@ -2,7 +2,6 @@ import axios from 'axios'
 const BASE_URL = 'http://localhost:3000/api/v1'
 
 export const signInUser = payload => {
-  debugger
   return axios.post(`${BASE_URL}/session`, payload)
 }
 
